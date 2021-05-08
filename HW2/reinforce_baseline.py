2c4cd0bc931cf9d850a53c2022627bfcb076b481
zonghan <steven.me05@nctu.edu.tw>
2021-05-08 13:57:00 +0800

    [HW2] finish reinforce algorithm

--- HW2/reinforce_baseline.py
+++ HW2/reinforce_baseline.py
@@ -16,7 +16,7 @@ import torch.optim.lr_scheduler as Scheduler
 # Define a useful tuple (optional)
 SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
 
-        
+
 class Policy(nn.Module):
     """
         Implement both policy network and the value network in one model
@@ -29,18 +29,25 @@ class Policy(nn.Module):
     """
     def __init__(self):
         super(Policy, self).__init__()
-        
+
         # Extract the dimensionality of state and action spaces
         self.discrete = isinstance(env.action_space, gym.spaces.Discrete)
         self.observation_dim = env.observation_space.shape[0]
         self.action_dim = env.action_space.n if self.discrete else env.action_space.shape[0]
         self.hidden_size = 128
-        
-        ########## YOUR CODE HERE (5~10 lines) ##########
 
-        
+        ########## YOUR CODE HERE (5~10 lines) ##########
+        self.shared_layer_1 = nn.Linear(self.observation_dim, self.hidden_size)
+        nn.init.kaiming_normal_(self.shared_layer_1.weight)
+        self.shared_layer_2 = nn.Linear(self.hidden_size, self.hidden_size)
+        nn.init.kaiming_normal_(self.shared_layer_2.weight)
+        self.action_layer = nn.Linear(self.hidden_size, self.action_dim)
+        nn.init.kaiming_normal_(self.action_layer.weight)
+        self.value_layer = nn.Linear(self.hidden_size, 1)
+        nn.init.kaiming_normal_(self.value_layer.weight)
+
         ########## END OF YOUR CODE ##########
-        
+
         # action & reward memory
         self.saved_actions = []
         self.rewards = []
@@ -48,14 +55,17 @@ class Policy(nn.Module):
     def forward(self, state):
         """
             Forward pass of both policy and value networks
-            - The input is the state, and the outputs are the corresponding 
+            - The input is the state, and the outputs are the corresponding
               action probability distirbution and the state value
             TODO:
                 1. Implement the forward pass for both the action and the state value
         """
-        
-        ########## YOUR CODE HERE (3~5 lines) ##########
 
+        ########## YOUR CODE HERE (3~5 lines) ##########
+        x = F.relu(self.shared_layer_1(state))
+        x = F.relu(self.shared_layer_2(x))
+        action_prob = F.softmax(self.action_layer(x), dim = -1)
+        state_value = self.value_layer(x)
 
         ########## END OF YOUR CODE ##########
 
@@ -65,17 +75,20 @@ class Policy(nn.Module):
     def select_action(self, state):
         """
             Select the action given the current state
-            - The input is the state, and the output is the action to apply 
+            - The input is the state, and the output is the action to apply
             (based on the learned stochastic policy)
             TODO:
                 1. Implement the forward pass for both the action and the state value
         """
-        
-        ########## YOUR CODE HERE (3~5 lines) ##########
 
+        ########## YOUR CODE HERE (3~5 lines) ##########
+        state = torch.from_numpy(state).float()
+        action_prob, state_value = self.forward(state)
+        m = Categorical(action_prob)
+        action = m.sample()
 
         ########## END OF YOUR CODE ##########
-        
+
         # save to action buffer
         self.saved_actions.append(SavedAction(m.log_prob(action), state_value))
 
@@ -90,19 +103,31 @@ class Policy(nn.Module):
                 2. Calculate the policy loss using the policy gradient
                 3. Calculate the value loss using either MSE loss or smooth L1 loss
         """
-        
+
         # Initialize the lists and variables
         R = 0
         saved_actions = self.saved_actions
-        policy_losses = [] 
-        value_losses = [] 
+        policy_losses = []
+        value_losses = []
         returns = []
 
         ########## YOUR CODE HERE (8-15 lines) ##########
+        for r in reversed(self.rewards):
+            R = r + gamma * R
+            returns.insert(0, R)
 
+        gamma_t = 1
+        for (log_prob, state_value), sample_return in zip(saved_actions, returns):
+            policy_loss = -gamma_t * (sample_return - state_value) * log_prob
+            policy_losses.append(policy_loss)
+            gamma_t *= gamma
+            value_loss = F.smooth_l1_loss(state_value, torch.tensor([sample_return]))
+            value_losses.append(value_loss)
+
+        loss = torch.stack(policy_losses).sum() + torch.stack(value_losses).sum()
 
         ########## END OF YOUR CODE ##########
-        
+
         return loss
 
     def clear_memory(self):
@@ -114,21 +139,21 @@ class Policy(nn.Module):
 def train(lr=0.01):
     '''
         Train the model using SGD (via backpropagation)
-        TODO: In each episode, 
+        TODO: In each episode,
         1. run the policy till the end of the episode and keep the sampled trajectory
         2. update both the policy and the value network at the end of episode
-    '''    
-    
+    '''
+
     # Instantiate the policy model and the optimizer
     model = Policy()
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    
+
     # Learning rate scheduler (optional)
     scheduler = Scheduler.StepLR(optimizer, step_size=100, gamma=0.9)
-    
+
     # EWMA reward for tracking the learning progress
     ewma_reward = 0
-    
+
     # run inifinitely many episodes
     for i_episode in count(1):
         # reset environment and episode reward
@@ -136,16 +161,29 @@ def train(lr=0.01):
         ep_reward = 0
         t = 0
         # Uncomment the following line to use learning rate scheduler
-        #scheduler.step()
-        
-        # For each episode, only run 9999 steps so that we don't 
+        scheduler.step()
+
+        # For each episode, only run 9999 steps so that we don't
         # infinite loop while learning
-        
+
         ########## YOUR CODE HERE (10-15 lines) ##########
-        
-        
+        for t in range(10000):
+            action = model.select_action(state)
+            next_state, reward, done, _ = env.step(action)
+            model.rewards.append(reward)
+            ep_reward += reward
+            if done:
+                break
+            state = next_state
+
+        optimizer.zero_grad()
+        loss = model.calculate_loss()
+        loss.backward()
+        optimizer.step()
+        model.clear_memory()
+
         ########## END OF YOUR CODE ##########
-            
+
         # update EWMA reward and log the results
         ewma_reward = 0.05 * ep_reward + (1 - 0.05) * ewma_reward
         print('Episode {}\tlength: {}\treward: {}\t ewma reward: {}'.format(i_episode, t, ep_reward, ewma_reward))
@@ -161,11 +199,11 @@ def train(lr=0.01):
 def test(name, n_episodes=10):
     '''
         Test the learned model (no change needed)
-    '''      
+    '''
     model = Policy()
-    
+
     model.load_state_dict(torch.load('./preTrained/{}'.format(name)))
-    
+
     render = True
 
     for i_episode in range(1, n_episodes+1):
@@ -181,15 +219,15 @@ def test(name, n_episodes=10):
                 break
         print('Episode {}\tReward: {}'.format(i_episode, running_reward))
     env.close()
-    
+
 
 if __name__ == '__main__':
     # For reproducibility, fix the random seed
-    random_seed = 20  
+    random_seed = 20
     lr = 0.01
     env = gym.make('CartPole-v0')
-    env.seed(random_seed)  
-    torch.manual_seed(random_seed)  
+    env.seed(random_seed)
+    torch.manual_seed(random_seed)
     train(lr)
     test('CartPole_0.01.pth')
 
